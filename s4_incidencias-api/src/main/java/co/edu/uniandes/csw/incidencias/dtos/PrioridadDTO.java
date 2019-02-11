c36633fc191bdc261b7e16cc3d40ffe4e3e81fcd
d.santamaria <d.santamaria@uniandes.edu.co>
2019-02-11 13:29:08 -0500

    Se agrega la clase PrioridadDTO

---
+++ s4_incidencias-api/src/main/java/co/edu/uniandes/csw/incidencias/dtos/PrioridadDTO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.dtos;
+
+/**
+ *
+ * @author estudiante
+ */
+public class PrioridadDTO {
+    
+    private String tipoPrioridad;
+    
+    public PrioridadDTO()
+    {
+        
+    }
+    
+    public String darTipoPrioridad()
+    {
+        return tipoPrioridad;
+    }
+    
+}
