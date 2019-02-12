a895cc4ff2438bac7a5065691d63a349a428d312
v.parrac <v.parrac@uniandes.edu.co>
2019-02-11 21:43:36 -0500

    DTO Usuario Técnico and Empleado

---
+++ s4_incidencias-api/src/main/java/co/edu/uniandes/csw/incidencias/dtos/EmpleadoDTO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.dtos;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author estudiante
+ */
+public class EmpleadoDTO extends UsuarioDTO implements Serializable{     
+
+    private static final Logger LOG = Logger.getLogger(EmpleadoDTO.class.getName());
+
+    
+    public EmpleadoDTO() {
+    }
+    
+}
