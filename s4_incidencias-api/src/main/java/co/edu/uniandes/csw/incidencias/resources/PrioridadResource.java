0787cd11256b7d40e8e3f56c7e6c50b359b30d73
d.santamaria <d.santamaria@uniandes.edu.co>
2019-02-13 11:37:13 -0500

    PrioridadResource y SlaResource

---
+++ s4_incidencias-api/src/main/java/co/edu/uniandes/csw/incidencias/resources/PrioridadResource.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.resources;
+
+import javax.ws.rs.Path;
+import co.edu.uniandes.csw.incidencias.dtos.IncidenciaDTO;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+/**
+ *
+ * @author estudiante
+ */
+
+@Path("Prioridades")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class PrioridadResource {
+      
+       
+private final static Logger LOGGER = Logger.getLogger(IncidenciaResource.class.getName());
+    
+    @POST
+    public IncidenciaDTO createPrioridadDTO(IncidenciaDTO incidencia){
+        return incidencia;
+    }
+   
+    
+    @PUT
+    public void UpdatePrioridadDTO(String estado){
+        
+    } 
+    
+}
---
+++ s4_incidencias-api/src/main/java/co/edu/uniandes/csw/incidencias/resources/SlaResource.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.resources;
+
+import javax.ws.rs.Path;
+import co.edu.uniandes.csw.incidencias.dtos.IncidenciaDTO;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+/**
+ *
+ * @author estudiante
+ */
+
+@Path("Sla")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class SlaResource {
+    
+       
+       
+private final static Logger LOGGER = Logger.getLogger(IncidenciaResource.class.getName());
+    
+    @POST
+    public IncidenciaDTO createPrioridadDTO(IncidenciaDTO incidencia){
+        return incidencia;
+    }
+   
+    
+    @PUT
+    public void UpdatePrioridadDTO(String estado){
+        
+    } 
+    
+    
+}
