5b0bf43e256b576ccc8dc5de19685973e8880d60
v.parrac <v.parrac@uniandes.edu.co>
2019-02-13 19:57:26 -0500

    Reviewed DepartamentoResource and UbibarionResource

--- s4_incidencias-api/src/main/java/co/edu/uniandes/csw/incidencias/resources/DepartamentoResource.java
+++ s4_incidencias-api/src/main/java/co/edu/uniandes/csw/incidencias/resources/DepartamentoResource.java
@@ -6,10 +6,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.logging.Logger;
 
-@Path('departamentos')
-@Produces('application/json')
-@Consumes('application/json')
+@Path("departamentos")
+@Produces("application/json")
+@Consumes("application/json")
 @RequestScoped
 public class DepartamentoResource {
 	
