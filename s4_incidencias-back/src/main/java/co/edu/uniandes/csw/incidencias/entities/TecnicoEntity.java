b95ef59b64c50efab33bd04f4584ffc3a4f0d0b2
v.parrac <v.parrac@uniandes.edu.co>
2019-02-18 13:17:21 -0500

    A

---
+++ s4_incidencias-back/src/main/java/co/edu/uniandes/csw/incidencias/entities/TecnicoEntity.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author estudiante
+ */
+
+@Entity
+public class TecnicoEntity extends BaseEntity implements Serializable{
+     private String usuario;
+    private String password;
+    private String cedula;
+    private String name;
+
+    
+    public TecnicoEntity(){
+        
+    }
+    
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return usuario;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.usuario = user;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
