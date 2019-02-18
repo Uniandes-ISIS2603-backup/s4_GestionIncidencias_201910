e139610bf01dd7950efb234b6f31e83f12296f28
d.santamaria <d.santamaria@uniandes.edu.co>
2019-02-18 12:50:46 -0500

    Creacion de entities Prioridad y Sla

---
+++ s4_incidencias-back/src/main/java/co/edu/uniandes/csw/incidencias/entities/SlaEntity.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author estudiante
+ */
+public class SlaEntity extends BaseEntity implements Serializable{
+    
+}
