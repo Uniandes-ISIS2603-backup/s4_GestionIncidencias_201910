c272cc58783b8af9dafff866db346d0b8c969ea1
d.santamaria <d.santamaria@uniandes.edu.co>
2019-02-20 10:42:01 -0500

    Adelanto de las pruebas de prioridad

---
+++ s4_incidencias-back/src/main/java/co/edu/uniandes/csw/incidencias/entities/PrioridadEntity.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author estudiante
+ */
+public class PrioridadEntity extends BaseEntity implements Serializable {
+    
+    private String tipoPrioridad;
+    private SlaEntity sla;
+    
+    public PrioridadEntity()
+    {
+        
+    }
+    
+
+    /**
+     * @return the tipoPrioridad
+     */
+    public String getTipoPrioridad() {
+        return tipoPrioridad;
+    }
+
+    /**
+     * @param tipoPrioridad the tipoPrioridad to set
+     */
+    public void setTipoPrioridad(String tipoPrioridad) {
+        this.tipoPrioridad = tipoPrioridad;
+    }
+
+    /**
+     * @return the sla
+     */
+    public SlaEntity getSla() {
+        return sla;
+    }
+
+    /**
+     * @param sla the sla to set
+     */
+    public void setSla(SlaEntity sla) {
+        this.sla = sla;
+    }
+    
+}
