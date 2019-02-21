6b6a16ae013b500e5a44f50c27e3bc9c6763d5e0
d.santamaria <d.santamaria@uniandes.edu.co>
2019-02-20 21:17:00 -0500

    Adelanto de las pruebas de prioridad

---
+++ s4_incidencias-back/src/main/java/co/edu/uniandes/csw/incidencias/ejb/SlaLogic.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.ejb;
+
+import co.edu.uniandes.csw.incidencias.entities.PrioridadEntity;
+import co.edu.uniandes.csw.incidencias.entities.SlaEntity;
+import co.edu.uniandes.csw.incidencias.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.incidencias.persistence.PrioridadPersistence;
+import co.edu.uniandes.csw.incidencias.persistence.SlaPersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Daniel Santamaria
+ */
+
+@Stateless
+public class SlaLogic {
+     
+    @Inject
+    private SlaPersistence persistence;
+    
+    public SlaEntity createPrioridad(SlaEntity sla)throws BusinessLogicException
+    {
+        if( persistence.findByName(sla.getIdSla()) != null)
+        {
+            throw new BusinessLogicException("Ya existe una prioridad Con el nombre \""+ sla.getId()+"\"" );
+        }
+        sla = persistence.create(sla);
+        return sla;
+    }
+    
+    
+    
+}
