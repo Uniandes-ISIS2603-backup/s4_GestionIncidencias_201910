dbf52e8d4e143a26df8f627ca1e0f188355fe088
Juan Guillermo Lobaton Galindo <jg.lobaton10@uniandes.edu.co>
2019-02-23 15:31:32 -0500

    Se agregaron las clases entity,persistence y el test de actuacion

---
+++ s4_incidencias-back/src/main/java/co/edu/uniandes/csw/incidencias/entities/ActuacionEntity.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author estudiante
+ */
+@Entity
+public class ActuacionEntity extends BaseEntity  implements Serializable {
+    
+    
+    private String fecha;
+    private String calificacion;
+    private String prioridad;
+    private String descripcion;
+
+    /**
+     * @return the fecha
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public String getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(String calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * @return the prioridad
+     */
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    /**
+     * @param prioridad the prioridad to set
+     */
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
---
+++ s4_incidencias-back/src/main/java/co/edu/uniandes/csw/incidencias/entities/IncidenciaEntity.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidencias.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author estudiante
+ */
+
+
+@Entity
+public class IncidenciaEntity implements  Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+}
